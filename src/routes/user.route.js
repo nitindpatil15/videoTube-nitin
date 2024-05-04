dffd9817d4c1ffc0d667799cee28a1a14e5513ec
nitindpatil15 <nitindpatil2002@gmail.com>
2024-05-04 14:54:34 +0530

    added new routes

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,11 +1,18 @@
 import { Router } from "express";
 import {
+  changeUserPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getUserWatchHistory,
   loginUser,
   logoutUser,
   refreshAccessToken,
   registerUser,
+  updateAvatar,
+  updateCoverImage,
+  updateUserDetails,
 } from "../controllers/user.controller.js";
-import { upload } from "../middleware/multer.middleware.js";
+import {upload}  from "../middleware/multer.middleware.js";
 import verifyJWT from "../middleware/auth.middleware.js";
 
 const router = Router();
@@ -30,6 +37,20 @@ router.route("/logout").post(
   verifyJWT, // we use next in method for avoiding any type of confusion for server
   logoutUser
 );
-router.route("refresh-token").post(refreshAccessToken);
+router.route("/refresh-token").post(refreshAccessToken);
+
+router.route("/change-password").post(verifyJWT, changeUserPassword);
+
+router.route("/getUser").post(verifyJWT,getCurrentUser)
+
+router.route("/update-User").patch(verifyJWT,updateUserDetails)
+
+router.route("/update-avatar").patch(verifyJWT,upload.single("avatar"), updateAvatar)
+
+router.route("/update-coverImage").patch(verifyJWT,upload.single("coverImage") ,updateCoverImage)
+
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+
+router.route("/watch-history").get(verifyJWT,getUserWatchHistory)
 
 export default router;
